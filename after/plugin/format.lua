c3b5b3ac8e3ac1f55aaedfdcef613715b141de7f
ymc182 <ymc182@gmail.com>
2023-07-07 16:26:04 +1000

    co-pilot

--- after/plugin/format.lua
+++ after/plugin/format.lua
@@ -1,2 +1,31 @@
 require("lsp-format").setup {}
 require("lspconfig").gopls.setup { on_attach = require("lsp-format").on_attach }
+
+require("lspconfig").tsserver.setup {
+    on_attach = require("lsp-format").on_attach,
+    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+}
+
+require("lspconfig").jsonls.setup {
+    on_attach = require("lsp-format").on_attach,
+    filetypes = { "json", "jsonc" },
+}
+
+require("lspconfig").html.setup {
+    on_attach = require("lsp-format").on_attach,
+    filetypes = { "html" },
+}
+
+--rust
+
+require("lspconfig").rust_analyzer.setup {
+    on_attach = require("lsp-format").on_attach,
+    filetypes = { "rust" },
+}
+
+--lua
+
+require("lspconfig").lua_ls.setup {
+    on_attach = require("lsp-format").on_attach,
+    filetypes = { "lua" },
+}
--- lua/ymc182/init.lua
+++ lua/ymc182/init.lua
@@ -2,3 +2,4 @@ require("ymc182.remap")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.cmd('set number')
